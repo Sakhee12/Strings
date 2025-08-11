cd25064a5f6eef5054bc63853647af068ebf09f7
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-08-11 13:18:59 +0530

    Add files via upload

---
+++ Hi.java
@@ -0,0 +1,16 @@
+package strings;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hi".hashCode());
+		System.out.println("hie");
+		String s1 = new String("Hi");
+		
+		System.out.println(s1.hashCode());
+	}
+
+}
